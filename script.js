24376924cce12d6cc21a3f3dbfa17b8e67098257
chibesa240 <chibesamumbi15@gmail.com>
2024-11-08 23:43:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode"); // Toggle between light and dark mode
+
+    // Optionally, you can also store the mode in localStorage to persist the mode across page reloads
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+
+// Load the theme from localStorage when the page loads
+window.addEventListener('load', () => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+});
+
+// Parallax Effect on Scroll
+document.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    const scrollPosition = window.scrollY;
+    header.style.backgroundPositionY = scrollPosition * 0.5 + "px";
+});
